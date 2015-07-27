8ebb8b2a444cc78d5b4d8a35d5eb64518396c1c6
fvafrCU <dominik.cullmann@forst.bwl.de>
2015-07-27 15:01:46 +0200

    found some weird stuff to set up a local cran
    But I don't use it.

---
+++ buildignore_local_cran.r
@@ -0,0 +1,38 @@
+localCRAN <- path.expand("~/lran")
+dir.create(localCRAN)
+contribDir <- file.path(localCRAN, "src", "contrib")
+dir.create(contribDir, recursive = TRUE)
+rVersion <- paste(unlist(getRversion())[1:2], collapse = ".")
+
+binPaths <- list(
+  win.binary = file.path("bin/windows/contrib", rVersion),
+  mac.binary = file.path("bin/macosx/contrib", rVersion),
+  mac.binary.mavericks = file.path("bin/macosx/mavericks/contrib", rVersion),
+  mac.binary.leopard = file.path("bin/macosx/leopard/contrib", rVersion)
+)
+
+binPaths <- lapply(binPaths, function(x) file.path(localCRAN, x))
+lapply(binPaths, function(path) {
+  dir.create(path, recursive = TRUE)
+})
+
+tar_ball <- "fvarug_0.0-2.tar.gz"
+file.copy(tar_ball, file.path(contribDir, tar_ball)
+)
+
+tools::write_PACKAGES(contribDir, type = "source")
+lapply(binPaths, function(path) {
+  tools::write_PACKAGES(path)
+})
+
+oldRepos <- getOption("repos")
+if(TRUE) {
+    message("we need a web servers' config for that") 
+    cranURI <- "http://10.33.50.43/lran/"
+} else {
+
+    cranURI <- paste("file://", normalizePath(localCRAN, winslash = "/"), sep = "")
+}
+options(repos = c( fvafrdebianCU = cranURI))
+
+install.packages("fvarug", type = "source")
