35a5381393c7b37aafb42cf70788eef259468f43
fvafrCU <dominik.cullmann@forst.bwl.de>
2015-07-28 11:20:48 +0200

    automate buildignore_local_cran.r
    formerly hardcoded the tarball

--- buildignore_local_cran.r
+++ buildignore_local_cran.r
@@ -1,3 +1,4 @@
+#!/usr/bin/Rscript --vanilla
 localCRAN <- path.expand("/var/www/lran")
 dir.create(localCRAN)
 contribDir <- file.path(localCRAN, "src", "contrib")
@@ -15,27 +16,19 @@ binPaths <- lapply(binPaths, function(x) file.path(localCRAN, x))
 lapply(binPaths, function(path) {
        dir.create(path, recursive = TRUE)
                  })
-
-tar_ball <- "fvarug_0.0-3.tar.gz"
-file.copy(tar_ball, file.path(contribDir, tar_ball))
+this_directory <- basename(getwd())
+tar_balls <- list.files(pattern = paste0(this_directory, ".*\\.tar\\.gz"))
+current_tar_ball <- tar_balls[length(tar_balls)]
+destination <- file.path(contribDir, current_tar_ball)
+file.copy(current_tar_ball, destination ,overwrite = TRUE)
+system(paste("chmod 644", destination))
 
 tools::write_PACKAGES(contribDir, type = "source")
 lapply(binPaths, function(path) {
        tools::write_PACKAGES(path)
                  })
 
-oldRepos <- getOption("repos")
 if (FALSE) {
-    if (TRUE) {
-        message("we need a web servers' config for that") 
-        cranURI <- "http://10.33.50.43/lran/"
-    } else {
-
-        cranURI <- paste("file://", normalizePath(localCRAN, winslash = "/"), sep = "")
-    }
-    options(repos = c( fvafrdebianCU = cranURI))
-    install.packages("fvarug", type = "source")
-} else {
     message("we need a web servers' config for that") 
     install.packages("fvarug", repos = "http://10.33.50.43/lran/", type = "source")
 }
